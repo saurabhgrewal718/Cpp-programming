f8061bb950d0b6643e2e1830208ed907133a44c3
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-05-22 23:39:51 +0530

    average using array

---
+++ average using array.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    float num[100], sum=0.0, average;
+
+    cout << "Enter the numbers of data: ";
+    cin >> n;
+
+    while (n > 100 || n <= 0)
+    {
+        cout << "Error! number should in range of (1 to 100)." << endl;
+        cout << "Enter the number again: ";
+        cin >> n;
+    }
+
+    for(i = 0; i < n; ++i)
+    {
+        cout << i + 1 << ". Enter number: ";
+        cin >> num[i];
+        sum += num[i];
+    }
+
+    average = sum / n;
+    cout << "Average = " << average;
+
+    return 0;
+}
+
+/*
+
+Enter the numbers of data: 6
+1. Enter number: 45.3
+2. Enter number: 67.5
+3. Enter number: -45.6
+4. Enter number: 20.34
+5. Enter number: 33
+6. Enter number: 45.6
+Average = 27.69
+
+*/
